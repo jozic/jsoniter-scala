183add6b90fa8aa5be761d55dd30470f4404593b
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-01-08 09:47:47 +0100

    Example of simplest validation

--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Examples.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Examples.scala
@@ -2,19 +2,52 @@ package com.github.plokhotnyuk.jsoniter_scala.examples
 
 import com.github.plokhotnyuk.jsoniter_scala.macros._
 import com.github.plokhotnyuk.jsoniter_scala.core._
+import java.time.LocalDate
 
-case class Device(id: Int, model: String)
+import scala.util.Try
 
-case class User(name: String, devices: Seq[Device])
+case class UserData(
+  user_id: String,
+  @stringified is_user_logged_in: Boolean,
+  checkin_date: LocalDate,
+  checkout_date: LocalDate
+) {
+  require(!is_user_logged_in || (user_id ne null), "missing `user_id` for logged in user")
+  require(checkout_date.isAfter(checkin_date), "`checkout_date` should be after `checkin_date`")
+}
 
 object Examples {
   def main(args: Array[String]): Unit = {
-    implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make[User](CodecMakerConfig())
+    implicit val codec: JsonValueCodec[UserData] = JsonCodecMaker.make[UserData](CodecMakerConfig())
 
-    val user = readFromArray("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
-    val json = writeToArray(User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X"))))
+    println(Try(readFromArray(
+      """
+        |{
+        |"user_id":"x",
+        |"is_user_logged_in":"true",
+        |"checkin_date":"2018-12-12",
+        |"checkout_date":"2019-12-13"
+        |}
+      """.stripMargin.getBytes("UTF-8"))))
 
-    println(user)
-    println(new String(json, "UTF-8"))
+    println(Try(readFromArray(
+      """
+        |{
+        |"user_id":"x",
+        |"is_user_logged_in":"true",
+        |"checkin_date":"2222-12-12",
+        |"checkout_date":"2019-12-13"
+        |}
+      """.stripMargin.getBytes("UTF-8"))))
+
+    println(Try(readFromArray(
+      """
+        |{
+        |"user_id":"x",
+        |"is_user_logged_in":"true",
+        |"checkin_date":"2222-12-12",
+        |"checkout_date":"2019-12-13"
+        |}
+      """.stripMargin.getBytes("UTF-8"))))
   }
 }
