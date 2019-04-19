a411a8ada3491e4765a707a0c85597e26ff10809
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2019-04-19 09:23:01 +0200

    Example of usage GraalVM native-image

--- jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Examples.scala
+++ jsoniter-scala-examples/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/examples/Examples.scala
@@ -1,5 +1,8 @@
 package com.github.plokhotnyuk.jsoniter_scala.examples
 
+import java.io.{FileInputStream, FileOutputStream}
+import java.util.zip.{GZIPInputStream, GZIPOutputStream}
+
 import com.github.plokhotnyuk.jsoniter_scala.macros._
 import com.github.plokhotnyuk.jsoniter_scala.core._
 
@@ -9,12 +12,25 @@ case class User(name: String, devices: Seq[Device])
 
 object Examples {
   def main(args: Array[String]): Unit = {
-    implicit val codec: JsonValueCodec[User] = JsonCodecMaker.make[User](CodecMakerConfig())
+    val startTime = System.currentTimeMillis
 
-    val user = readFromArray("""{"name":"John","devices":[{"id":1,"model":"HTC One X"}]}""".getBytes("UTF-8"))
-    val json = writeToArray(User(name = "John", devices = Seq(Device(id = 2, model = "iPhone X"))))
+    case class OldReport(campaignId: String, campaignName: String)
+    case class NewReport(reportDate: String, profileId: Long, data: Array[OldReport])
 
-    println(user)
-    println(new String(json, "UTF-8"))
+    implicit val codec: JsonValueCodec[Array[OldReport]] = JsonCodecMaker.make(CodecMakerConfig())
+    implicit val codec2: JsonValueCodec[NewReport] = JsonCodecMaker.make(CodecMakerConfig())
+    val fis: FileInputStream = new FileInputStream("old.json.gz")
+    val gis: GZIPInputStream = new GZIPInputStream(fis)
+
+    val report = readFromStream(gis)(codec)
+    val dataToCompress = NewReport("20190101", 123, report)
+
+    val fos: FileOutputStream = new FileOutputStream("new.json.gz")
+    val gos: GZIPOutputStream = new GZIPOutputStream(fos)
+    writeToStream(dataToCompress, gos)(codec2)
+    gos.close()
+
+    val stopTime = System.currentTimeMillis
+    println(stopTime - startTime)
   }
 }
